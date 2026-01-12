8c328dffd8539de790c698773016aeeb37ec1f9f
Gachikoi <k1ss.sakura@outlook.com>
2026-01-12 23:24:42 +0800

    build(web): 修改 cloudflare 适配器

--- packages/web/svelte.config.js
+++ packages/web/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,16 +9,9 @@ const config = {
 
 	kit: {
 		adapter: adapter({
-			// See below for an explanation of these options
 			routes: {
 				include: ['/*'],
 				exclude: ['<all>']
-			},
-			platformProxy: {
-				configPath: 'wrangler.toml',
-				environment: undefined,
-				experimentalJsonConfig: false,
-				persist: false
 			}
 		})
 	}
